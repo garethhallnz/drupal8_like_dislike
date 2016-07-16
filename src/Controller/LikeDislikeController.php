86c2c96aba181a4df185167184e7513099a5a86a
heykarthikwithu <heykarthikwithu@gmail.com>
2016-07-16 12:42:08 +0530

    Show status of click, if already liked/disliked.

--- src/Controller/LikeDislikeController.php
+++ src/Controller/LikeDislikeController.php
@@ -64,13 +64,16 @@ class LikeDislikeController extends ControllerBase {
     $return = '';
     $response = new AjaxResponse();
 
+    // Decode the url data
     $decode_data = json_decode(base64_decode($data));
 
+    // Load the entity content.
     $entity_data = \Drupal::entityTypeManager()
       ->getStorage($decode_data->entity_type)
       ->load($decode_data->entity_id);
     $field_name = $decode_data->field_name;
 
+    // Get the users who already clicked on this particular content.
     $users = json_decode($entity_data->$field_name->clicked_by);
     if ($users == NULL) {
       $users = new \stdClass();
@@ -78,38 +81,25 @@ class LikeDislikeController extends ControllerBase {
     }
 
     $user = \Drupal::currentUser()->id();
+    // If user is ananomous, ask him to register/login.
     if ($user == 0) {
       $destination = $this->requestStack->getCurrentRequest()->get('like-dislike-redirect');
       user_cookie_save(['destination' => $destination]);
-      $options = array(
-        'attributes' => array(
-          'class' => array(
-            'use-ajax',
-            'login-popup-form',
-          ),
-          'data-dialog-type' => 'modal',
-        ),
-      );
-      $user_register = Url::fromRoute('user.register')->setOptions($options);
-      $user_login = Url::fromRoute('user.login')->setOptions($options);
-      $register = Link::fromTextAndUrl(t('Register'), $user_register)->toString();
-      $login = Link::fromTextAndUrl(t('Log in'), $user_login)->toString();
-      $content = array(
-        'content' => array(
-          '#markup' => "Only logged in users are allowed to like/dislike. \n Visit ".$register ." | " . $login,
-        ),
-      );
-      $user_login_register = \Drupal::service('renderer')->render($content);
+      $user_login_register = $this->like_dislike_login_register();
       $dialog_library['#attached']['library'][] = 'core/drupal.dialog.ajax';
       $response->setAttachments($dialog_library['#attached']);
       return $response->addCommand(new OpenModalDialogCommand('Like/Dislike', $user_login_register));
     }
 
+    // Update content, based on like/dislike.
     $already_clicked = key_exists($user, array_keys((array) $users));
     if ($clicked == 'like') {
       if (!$already_clicked) {
         $entity_data->$field_name->likes++;
         $users->$user = 'like';
+      }
+      else {
+        return $this->like_dislike_status($response);
       }
       $return = $response->addCommand(new HtmlCommand('#like', $entity_data->$field_name->likes));
     }
@@ -118,10 +108,51 @@ class LikeDislikeController extends ControllerBase {
         $entity_data->$field_name->dislikes--;
         $users->$user = "dislike";
       }
+      else {
+        return $this->like_dislike_status($response);
+      }
       $return = $response->addCommand(new HtmlCommand('#dislike', $entity_data->$field_name->dislikes));
     }
     $entity_data->$field_name->clicked_by = json_encode($users);
     $entity_data->save();
+    return $return;
+  }
+
+  /**
+   * Get the login and Registration options for ananomous user.
+   *
+   * @return mixed
+   */
+  protected function like_dislike_login_register() {
+    $options = array(
+      'attributes' => array(
+        'class' => array(
+          'use-ajax',
+          'login-popup-form',
+        ),
+        'data-dialog-type' => 'modal',
+      ),
+    );
+    $user_register = Url::fromRoute('user.register')->setOptions($options);
+    $user_login = Url::fromRoute('user.login')->setOptions($options);
+    $register = Link::fromTextAndUrl(t('Register'), $user_register)->toString();
+    $login = Link::fromTextAndUrl(t('Log in'), $user_login)->toString();
+    $content = array(
+      'content' => array(
+        '#markup' => "Only logged in users are allowed to like/dislike. \n Visit ".$register ." | " . $login,
+      ),
+    );
+    return \Drupal::service('renderer')->render($content);
+  }
+
+  /**
+   * Respond with the status, if user already liked/disliked.
+   *
+   * @param AjaxResponse $response
+   * @return AjaxResponse
+   */
+  protected function like_dislike_status(AjaxResponse $response) {
+    $return = $response->addCommand(new HtmlCommand('#like_dislike_status', 'Already liked/disliked..!'));
     return $return;
   }
 
